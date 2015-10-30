cedb443030401d44c2d212e568d665bdc72392f3
daniel anggrianto <danggrianto@50onred.com>
2015-10-30 18:16:43 -0400

    remove theme from gitignore

---
+++ content/themes/symmetric/config.js
@@ -0,0 +1,10 @@
+// Theme Config, check the documentation before edit this file
+
+var theme_config = {
+  twitter_app_id: '658714870372827136', // Widget id created on https://twitter.com/settings/widgets, correct example: 658074580351873024
+	  twitter_username: 'orangindoo', //Without @ and lowercase, correct example: wordpress
+  twitter_limit: 5, // Number of tweets to show
+  flickr_id: '46776802@N03', // You can get it here: http://idgettr.com/
+  flickr_photos_limit: 6, // How many photos you want to show
+  disqus_shortname: 'orangindo', // You can create your own Disqus Comments here: https://disqus.com/admin/create/
+};
