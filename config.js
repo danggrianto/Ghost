764c4b3b43cfaed56ed7f87a2db6fc0afdbc3bd4
daniel anggrianto <danggrianto@50onred.com>
2015-10-30 17:41:15 -0400

    add config for development

--- config.js
+++ config.js
@@ -38,9 +38,26 @@ config = {
             port: '2368'
         },
         paths: {
-            contentPath: path.join(__dirname, '/')
+            contentPath: path.join(__dirname, '/content/')
         }
-    }
+    },
+    development: {
+        url: 'http://localhost:2368',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
+    },
 };
 
 module.exports = config;
